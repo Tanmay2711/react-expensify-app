ba924e74b38174c66e5b370c30e2d089a149921e
Tanmay Vartak <tanmay.vartak@ey.com>
2020-07-21 05:33:23 +0530

    Added set_expenses actions

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -1,23 +1,25 @@
 const expenseReducerDefaultState = [];
 
 export default (state = expenseReducerDefaultState, action) => {
-    switch(action.type){
-        case 'ADD_EXPENSE':
-            return [...state,action.expense];
-        case 'REMOVE_EXPENSE':
-            return state.filter(({id}) => id !== action.id);
-        case 'EDIT_EXPENSE':
-            return state.map((expense) => {
-                if(expense.id === action.id){
-                    return {
-                        ...expense,
-                        ...action.expenseToEdit
-                    }
-                }else {
-                    return expense;
-                }
-            });
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      return state.filter(({ id }) => id !== action.id);
+    case "EDIT_EXPENSE":
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.expenseToEdit,
+          };
+        } else {
+          return expense;
+        }
+      });
+    case "SET_EXPENSES":
+      return action.expenses;
+    default:
+      return state;
+  }
+};
