8463e94c6e4982e9bddc6546821565c9c0d8c997
Tanmay Vartak <tanmay.vartak@ey.com>
2020-07-26 17:33:21 +0530

    Updated History path

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Header from './../components/Header';
-import AddExpense from './../components/AddExpense';
-import EditExpense from './../components/EditExpense';
-import ExpenseDashboard from './../components/ExpenseDashboard';
-import Help from './../components/Help';
-import NotFound from './../components/NotFound';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Header from "./../components/Header";
+import AddExpense from "./../components/AddExpense";
+import EditExpense from "./../components/EditExpense";
+import ExpenseDashboard from "./../components/ExpenseDashboard";
+import Help from "./../components/Help";
+import NotFound from "./../components/NotFound";
+
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" component={ExpenseDashboard} exact={true}></Route>
-                <Route path="/create" component={AddExpense}></Route>
-                <Route path="/edit/:id" component={EditExpense}></Route>
-                <Route path="/help" component={Help}></Route>
-                <Route component={NotFound}></Route>
-            </Switch>
-        </div>
-    </BrowserRouter>
+  <Router history={history}>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboard} exact={true}></Route>
+        <Route path="/create" component={AddExpense}></Route>
+        <Route path="/edit/:id" component={EditExpense}></Route>
+        <Route path="/help" component={Help}></Route>
+        <Route component={NotFound}></Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default AppRouter;
